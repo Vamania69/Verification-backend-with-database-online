7792d023107928ce31b4fe25dd56f5863c4462f2
varun agrawal <90302777+Vamania69@users.noreply.github.com>
2023-03-02 11:13:42 +0530

    config changes

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ dotenv.config();
 // to read all the values of body
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    success: 1,
+    message: "The server is running fine",
+  });
+});
 //sample rotues to varify the endpoints
 app.get("/api", (req, res) => {
   res.json({
@@ -21,6 +27,6 @@ app.post("/api/users", userRouter);
 app.get("/apis", userRouter);
 
 //port location
-app.listen(5002, () => {
+app.listen(5002 || process.env.APP_PORT, () => {
   console.log("This server is running at localhost :", process.env.APP_PORT);
 });
